7012ce97345aec2219612076cd53334c6f5bd6b2
ramadhanbridge <nyiringondoramadhan@gmail.com>
2019-08-26 08:45:33 +0200

    add setup

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 
 import express from 'express';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
-})
+app.get('/', (req, res) => res.status(200).send({ message: 'YAY! Congratulations! Your first endpoint is working' }));
+const man = () => hi;
 
-app.listen(5000)
+app.listen(5000);
 console.log('app running on port ', 5000);
+console.log(
+  '300',
+);
+const ma = () => hi;
